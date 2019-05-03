b35c33acacace92fded7f821e3a60f9028e40a55
Ludvig Sundström <lud.sund@gmail.com>
2019-05-03 18:52:47 +0200

    Correct fix version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trainer "1.3.3-SNAPSHOT"
+(defproject trainer "1.3.0-SNAPSHOT"
   :description "Personal fitness application"
   :url "https://github.com/lsund/trainer"
   :min-lein-version "2.7.0"
